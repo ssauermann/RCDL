54ee337441691132ad782275f860034a02549471
Alexander Frank <alexander.pas@online.de>
2015-01-17 17:00:01 +0100

    Update: Repeater and Redstone added

--- RCDL/src/com/tree_bit/com/rcdl/core/Redstone.java
+++ RCDL/src/com/tree_bit/com/rcdl/core/Redstone.java
@@ -1,5 +1,22 @@
 package com.tree_bit.com.rcdl.core;
 
-public class Redstone {
+/**
+ * An Object of this class contains all Information about Redstone
+ *
+ * @author Alexander
+ *
+ */
+public class Redstone extends Blocks {
+
+	public Redstone(int power) {
+		super(55, power);
+	}
+
+	/**
+	 * Standard Constructor which sets the power to 0
+	 */
+	public Redstone() {
+		super(55, 0);
+	}
 
 }
