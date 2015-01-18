0f87ee4fac6a7c934eb8d0a95570cdb901996874
ssauermann <SaschaSauermann@gmx.de>
2015-01-18 19:57:05 +0100

    + getLength() + javadoc

--- RCDL/src/com/tree_bit/com/rcdl/core/Tuple3.java
+++ RCDL/src/com/tree_bit/com/rcdl/core/Tuple3.java
@@ -107,20 +107,64 @@ public class Tuple3 implements Comparable<Tuple3> {
 		this.z = z;
 	}
 
+	/**
+	 * Adds an other vector to this one.
+	 *
+	 * @param t
+	 *            <b>Tuple3</b> second vector
+	 * @return <b>Tuple3</b> sum
+	 */
 	public Tuple3 add(Tuple3 t) {
 		return new Tuple3(x + t.x, y + t.y, z + t.z);
 	}
 
+	/**
+	 * Negates this vector.
+	 *
+	 * @return <b>Tuple3</b> negated vector
+	 */
 	public Tuple3 negate() {
 		return new Tuple3(-x, -y, -z);
 	}
 
+	/**
+	 * Multiplies this vector with an integer.
+	 *
+	 * @param m
+	 *            <b>int</b> factor
+	 * @return <b>Tuple3</b> product
+	 */
 	public Tuple3 multiply(int m) {
 		return new Tuple3(x * m, y * m, z * m);
 	}
 
+	/**
+	 * Divides this vector by an integer.
+	 *
+	 * @param m
+	 *            <b>int</b> divisor
+	 * @return <b>Tuple3</b> quotient
+	 */
 	public Tuple3 divide(int m) {
 		return new Tuple3(x / m, y / m, z / m);
+	}
+
+	/**
+	 * Returns the length of this vector.
+	 *
+	 * @return <b>double</b> length
+	 */
+	public double getLength() {
+		return Math.sqrt((x * x) + (y * y) + (z * z));
+	}
+
+	/**
+	 * Returns the volume of the cuboid spanned by this vector.
+	 *
+	 * @return <b>Tuple3</b> volume
+	 */
+	public int getVolume() {
+		return getX() * getY() * getZ();
 	}
 
 	@Override
@@ -151,9 +195,5 @@ public class Tuple3 implements Comparable<Tuple3> {
 		int valThis = ((getX() * getX()) + (getY() * getY()) + (getZ() * getZ()));
 		int valOther = ((o.getX() * o.getX()) + (o.getY() * o.getY()) + (o.getZ() * o.getZ()));
 		return valThis - valOther;
-	}
-
-	public int getVolume() {
-		return getX() * getY() * getZ();
 	}
 }
