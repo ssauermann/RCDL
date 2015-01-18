08f474c24402d5bdf310f9b821042ae3600d9ea3
Alexander Frank <alexander.pas@online.de>
2015-01-18 17:41:09 +0100

    Hotfix for StandingSigns

--- RCDL/src/com/tree_bit/com/rcdl/blocks/StandingSign.java
+++ RCDL/src/com/tree_bit/com/rcdl/blocks/StandingSign.java
@@ -19,12 +19,13 @@ public class StandingSign extends Blocks {
 		super(63, orientation.getOrientation());
 		if (text.length > 4)
 			throw new IllegalArgumentException("Too much text for a sign. String array is too big.");
+		this.text = text;
 	}
 
 	public enum Orientation {
 		South(0), SouthSouthWest(1), SouthWest(2), WestSouthWest(3), West(4), WestNorthWest(5), NorthWest(
 				6), NorthNorthWest(7), North(8), NorthNorthEast(9), NorthEast(10), EastNorthEast(11), East(
-				12), EastSouthEast(13), SouthEast(14), SouthSouthEast(15);
+						12), EastSouthEast(13), SouthEast(14), SouthSouthEast(15);
 
 		private int value;
 
