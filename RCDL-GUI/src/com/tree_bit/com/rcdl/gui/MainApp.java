ff82c1d677ff34c88859333977768de780d23c8b
Alexander Frank <alexander.pas@online.de>
2015-01-20 17:30:42 +0100

    Update: referring to the controller added, changes in resize policy

--- RCDL-GUI/src/com/tree_bit/com/rcdl/gui/MainApp.java
+++ RCDL-GUI/src/com/tree_bit/com/rcdl/gui/MainApp.java
@@ -3,8 +3,10 @@ package com.tree_bit.com.rcdl.gui;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -13,6 +15,9 @@ public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private AnchorPane editingWindow;
+	@FXML
+	private Tab tab1;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,23 +28,6 @@ public class MainApp extends Application {
 		initRootLayout();
 
 		showEditingWindow();
-	}
-
-	private void showEditingWindow() {
-		try
-		{
-			// Load person overview.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("EditingWindow.fxml"));
-			AnchorPane personOverview = (AnchorPane) loader.load();
-
-			// Set person overview into the center of root layout.
-			rootLayout.setCenter(personOverview);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
 	}
 
 	private void initRootLayout() {
@@ -54,6 +42,28 @@ public class MainApp extends Application {
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void showEditingWindow() {
+		try
+		{
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("EditingWindow.fxml"));
+			editingWindow = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(editingWindow);
+
+			// Give the controller access to the main app.
+			EditingWindowController controller = loader.getController();
+			controller.setMainApp(this);
+
 		}
 		catch (IOException e)
 		{
