a4019681148a0d2ad40e6817f16984dd95c49bb1
Alexander Frank <alexander.pas@online.de>
2015-02-20 20:54:22 +0100

    Replaced existing Gui-tests

--- GUI/src/main/java/com/tree_bit/rcdl/gui/MainApp.java
+++ GUI/src/main/java/com/tree_bit/rcdl/gui/MainApp.java
@@ -3,68 +3,84 @@ package com.tree_bit.rcdl.gui;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private BorderPane rootLayout;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private AnchorPane editingWindow;
+	@FXML
+	private Tab tab1;
 
-    @Override
-    public void start(Stage primaryStage) {
+	@Override
+	public void start(Stage primaryStage) {
 
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("RCDL - Redstone Circuitry Description Language");
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("RCDL - Redstone Circuitry Description Language");
 
-        this.initRootLayout();
+		initRootLayout();
 
-        this.showEditingWindow();
-    }
+		showEditingWindow();
+	}
 
-    private void showEditingWindow() {
-        try {
-            // Load person overview.
-            final FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("EditingWindow.fxml"));
-            final AnchorPane personOverview = (AnchorPane) loader.load();
+	private void initRootLayout() {
+		try
+		{
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-            // Set person overview into the center of root layout.
-            this.rootLayout.setCenter(personOverview);
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-    private void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            final FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("RootLayout.fxml"));
-            this.rootLayout = (BorderPane) loader.load();
+	private void showEditingWindow() {
+		try
+		{
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("EditingWindow.fxml"));
+			editingWindow = (AnchorPane) loader.load();
 
-            // Show the scene containing the root layout.
-            final Scene scene = new Scene(this.rootLayout);
-            this.primaryStage.setScene(scene);
-            this.primaryStage.show();
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(editingWindow);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+			// Give the controller access to the main app.
+			EditingWindowController controller = loader.getController();
+			controller.setMainApp(this);
 
-    /**
-     * Returns the main stage.
-     *
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return this.primaryStage;
-    }
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * Returns the main stage.
+	 *
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
 }
