4ca82fa3c0c598d9da9638363ee28670a8b36dc7
S. Sauermann <SaschaSauermann@gmx.de>
2015-02-20 22:55:25 +0100

    - Extracted BlockAPI to own project and repository.

--- BlockAPI/src/main/java/com/tree_bit/rcdl/blocks/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Classes giving access to Minecraft blocks and their special data value
- * behavior.
- */
-@NonNullByDefault
-package com.tree_bit.rcdl.blocks;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
--- BlockAPI/src/main/java/com/tree_bit/rcdl/schematic/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes to access a Minecraft schematic or world file.
- */
-@NonNullByDefault
-package com.tree_bit.rcdl.schematic;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
